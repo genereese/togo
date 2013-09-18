2f7753b44535b786e7ab7024dc1be1d835fb724a
Gene Reese <gene.reese@xirsix.com>
2013-09-18 07:11:15 -0500

    Changed directory structure and added a build script to generate the togo RPM from source.

---
+++ build-togo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo
+echo "Checking build dependencies..."
+echo
+
+rpm -q rpm-build python-sqlobject
+
+if [ $? -eq 1 ]; then
+	echo
+	echo "ERROR: Build dependencies not met."
+	echo
+	echo "  Please ensure the above packages are installed before building."
+	echo
+else
+	echo
+	echo "Attempting to building the togo RPM..."
+	./root/usr/local/bin/togo -a
+	echo
+fi
