b734d76f5ef31b5580f3743d6ef727f6bf668376
Gene Reese <gene.reese@xirsix.com>
2016-09-30 07:59:01 -0500

    - Minor changes to logging output and added 'rsync' as a prerequisite for the build-togo.sh script.

--- build-togo.sh
+++ build-togo.sh
@@ -4,14 +4,14 @@ echo
 echo "Checking build dependencies..."
 echo
 
-rpm -q rpm-build python-sqlobject
+rpm -q rpm-build python-sqlobject rsync
 
 if [ $? -ne 0 ]; then
 	echo
 	echo "ERROR: Build dependencies not met."
 	echo
-	echo "  Please ensure that the 'rpm-build' and 'python-sqlobject' rpms"
-	echo "  are installed before building."
+	echo "  Please ensure that the 'rpm-build','python-sqlobject', and 'rysnc'"
+	echo "  rpms are installed before building."
 	echo
 	echo "  If you have satisfied these requirements via some other means"
 	echo "  (easy_install, etc.) you may try to build the RPM using:"
