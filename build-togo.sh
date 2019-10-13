ccf180b91c43cb0791900e6f20f6105218d3d20e
Gene Reese <greese@xirsix.com>
2019-10-13 10:45:08 -0500

    Updated togo for explicity python2 usage

--- build-togo.sh
+++ build-togo.sh
@@ -4,17 +4,23 @@ echo
 echo "Checking build dependencies..."
 echo
 
-rpm -q rpm-build python-sqlobject rsync
+rpm -q rpm-build rsync python2 python-sqlobject
 
 if [ $? -ne 0 ]; then
 	echo
 	echo "ERROR: Build dependencies not met."
 	echo
-	echo "  Please ensure that the 'rpm-build','python-sqlobject', and 'rsync'"
+	echo "  Please ensure that the 'rpm-build', 'rsync', 'python2', and 'python-sqlobject'"
 	echo "  rpms are installed before building."
 	echo
-	echo "  If you have satisfied these requirements via some other means"
-	echo "  (easy_install, etc.) you may try to build the RPM using:"
+	echo "  NOTE:"
+	echo "    If your RPM repositories do not include 'python-sqlobject', you may install it"
+	echo "    using pip:"
+	echo
+	echo "    'pip2 install sqlobject'"
+	echo
+	echo "  Once you are sure you have satisfied these requirements using either method,"
+	echo "  you may build the RPM using the following command:"
 	echo
 	echo "  ./root/usr/bin/togo build package"
 	echo
