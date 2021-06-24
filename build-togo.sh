0c47ffed0be22ac3c64512de7b12ec56b3ec15b5
Gene Reese <greese@xirsix.com>
2021-06-24 03:10:19 -0500

    Modified togo to utilize python 3

--- build-togo.sh
+++ build-togo.sh
@@ -4,20 +4,20 @@ echo
 echo "Checking build dependencies..."
 echo
 
-rpm -q rpm-build rsync python2 python2-sqlobject
+rpm -q rpm-build rsync python3 python3-sqlobject
 
 if [ $? -ne 0 ]; then
 	echo
 	echo "ERROR: Build dependencies not met."
 	echo
-	echo "  Please ensure that the 'rpm-build', 'rsync', 'python2', and 'python2-sqlobject'"
+	echo "  Please ensure that the 'rpm-build', 'rsync', 'python3', and 'python3-sqlobject'"
 	echo "  rpms are installed before building."
 	echo
 	echo "  NOTE:"
-	echo "    If your RPM repositories do not include 'python2-sqlobject', you may install it"
+	echo "    If your RPM repositories do not include 'python3-sqlobject', you may install it"
 	echo "    using pip:"
 	echo
-	echo "    'pip2 install sqlobject'"
+	echo "    'pip install sqlobject'"
 	echo
 	echo "  Once you are sure you have satisfied these requirements using either method,"
 	echo "  you may build the RPM using the following command:"
